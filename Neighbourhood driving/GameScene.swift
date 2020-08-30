47efe73b6252119e13d5ccd4c2b8afc8433dc4ac
Testing_author <testing_beta@mail.com>
2020-08-30 13:09:03 +0300

    Inserting the scoreLabel and fuelLabel. mplementing the methods which will allow us to find out what object have contacted with others. Added isGameOver property to end the player's game.

--- Neighbourhood driving/GameScene.swift
+++ Neighbourhood driving/GameScene.swift
@@ -17,24 +17,50 @@ enum CollisionTypes: UInt32 {
     case finish = 32
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var player: SKSpriteNode!
     var lastTouchPosition: CGPoint?
     
     var motionManager: CMMotionManager?
+    var isGameOver = false
+    
+    var scoreLabel: SKLabelNode!
+    
+    var fuelLabel: SKLabelNode! //make it an animation
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
-            background.position = CGPoint(x: 512, y: 384)
-            background.blendMode = .replace
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
-            
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.position = CGPoint(x: 16, y:16)
+        scoreLabel.zPosition = 2
+        addChild(scoreLabel)
+        
+        fuelLabel = SKLabelNode(fontNamed: "Chalkduster")
+        fuelLabel.text = "Fuel left: 0"
+        fuelLabel.horizontalAlignmentMode = .left
+        fuelLabel.position = CGPoint(x: 16, y:16)
+        fuelLabel.zPosition = 2
+        addChild(fuelLabel)
+        
         loadLevel()
         createPlayer()
         
         physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self //telling us when a collision happened
         
         motionManager = CMMotionManager()
         motionManager?.startAccelerometerUpdates()
@@ -79,12 +105,12 @@ class GameScene: SKScene {
                     
                     node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2.2) //ensure that the player will not fall when simply touching the cracks
                     node.physicsBody?.isDynamic = false
-                   
+                    
                     node.physicsBody?.categoryBitMask = CollisionTypes.hole.rawValue
                     node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue //we are notified when player touches it
                     node.physicsBody?.collisionBitMask = 0 //does not bounce
                     
-                     node.position = position
+                    node.position = position
                     addChild(node)
                     
                 } else if letter == "s" {
@@ -128,7 +154,7 @@ class GameScene: SKScene {
                     
                     let node = SKSpriteNode(imageNamed: "finish_1")
                     node.name = "finish"
-
+                    
                     node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
                     node.physicsBody?.isDynamic = false
                     
@@ -140,8 +166,8 @@ class GameScene: SKScene {
                     addChild(node)
                     
                 } else if letter == " " {
-                //these are the empty spaces which do nothing
-                
+                    //these are the empty spaces which do nothing
+                    
                 } else {
                     fatalError("Uknown level letter: \(letter)")
                 }
@@ -157,14 +183,14 @@ class GameScene: SKScene {
         
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2.2)
         player.physicsBody?.allowsRotation = false
-      
+        
         /////////////////////////////////////////////////////////////////////////////////////
         //TODO: make the moto rotate in the direction it is currently heading
-//        player.physicsBody?.allowsRotation = true
-//
-//        let angleRandomizer = Int.random (in: 1...360) //adjust randomizer depending on the method of tilting the display
-//        player.run(SKAction.repeat(SKAction.rotate(byAngle: CGFloat(angleRandomizer), duration: 1), count: 1))  //adjust to have th necessary angle for each rotation
-//        player.physicsBody?.isDynamic = false
+        //        player.physicsBody?.allowsRotation = true
+        //
+        //        let angleRandomizer = Int.random (in: 1...360) //adjust randomizer depending on the method of tilting the display
+        //        player.run(SKAction.repeat(SKAction.rotate(byAngle: CGFloat(angleRandomizer), duration: 1), count: 1))  //adjust to have th necessary angle for each rotation
+        //        player.physicsBody?.isDynamic = false
         //////////////////
         
         player.physicsBody?.linearDamping = 0.5
@@ -196,6 +222,7 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
+        guard isGameOver == false else { return }
         
         #if targetEnvironment(simulator)
         if let lastTouchPosition = lastTouchPosition {
@@ -208,5 +235,42 @@ class GameScene: SKScene {
             physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.y * -50, dy: accelerometerData.acceleration.x * 50)
         }
         #endif
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+           guard let nodeB = contact.bodyB.node else { return }
+        
+        if nodeA == player {
+            playerCollided(with: nodeB)
+        } else if nodeB == player {
+            playerCollided(with: nodeA)
+        }
+    }
+    
+    func playerCollided(with node: SKNode) {
+        if node.name == "hole" {
+            player.physicsBody?.isDynamic = false
+            isGameOver = true
+            score -= 1
+            
+            let move = SKAction.move(to: node.position, duration: 0.5)
+            let scale = SKAction.scale(to: 0.0001, duration: 0.5)
+            let remove = SKAction.removeFromParent()
+            let sequence = SKAction.sequence([move, scale, remove])
+            
+            player.run(sequence) { [weak self] in
+                self?.createPlayer()
+                self?.isGameOver = false
+            }
+        } else if node.name == "star" {
+            node.removeFromParent()
+            score += 1
+        } else if node.name == "fuel" {
+            //add code to fill fuel bar
+            node.removeFromParent()
+            score += 1
+        } else if node.name == "finish" {
+            //add code to move to next level
+        }
     }
 }
